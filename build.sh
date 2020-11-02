e3f2eb9756975f8805b89d16e5e96842574491fa
Sebastian Waldbauer <waldbauer@cert.at>
2020-11-02 16:40:00 +0100

    Fixed paths
    Signed-off-by: Sebastian Waldbauer <waldbauer@cert.at>

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 build_date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-git_ref_core=$(cd ./intelmq && git rev-parse --short HEAD)
-git_ref_manager=$(cd ./intelmq-manager && git rev-parse --short HEAD)
-build_version="1.1"
+git_ref_core=$(cd ../intelmq && git rev-parse --short HEAD)
+git_ref_manager=$(cd ../intelmq-manager && git rev-parse --short HEAD)
+build_version="1.0"
 echo Building new IntelMQ-Image v$build_version
 echo Core      : $git_ref_core
 echo Manager   : $git_ref_manager
 echo Build_date: $build_date
 
+cp -r ../intelmq ./intelmq
+cp -r ../intelmq-manager ./intelmq-manager
+
 docker build --no-cache --build-arg BUILD_DATE=$build_date \
     --build-arg VCS_REF="$git_ref_core, $git_ref_manager" \
     --build-arg BUILD_VERSION=$build_version \
     -f Dockerfile \
-    -t intelmq-full .
+    -t intelmq-full:$build_version .
