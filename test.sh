de9012c694ece49308719473e837c331a1a44bd7
Sebastian Waldbauer <waldbauer@cert.at>
2020-11-03 13:50:29 +0100

    Fixed start param
    Signed-off-by: Sebastian Waldbauer <waldbauer@cert.at>

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo docker run -d --name="intelmq_test_redis" -p 6379:6379 -v ~/intelmq-docker/example_config/redis/redis.conf:/redis.conf redis:latest
-sudo docker run -d --name="intelmq_test_intelmq" \ 
-    -v ~/intelmq-docker/example_config/intelmq/etc:/opt/intelmq/etc \
+sudo docker run -d --name="intelmq_test_intelmq" -v ~/intelmq-docker/example_config/intelmq/etc:/opt/intelmq/etc \
     -v ~/intelmq-docker/example_config/intelmq-manager:/opt/intelmq-manager/config \
     -v ~/intelmq-docker/intelmq_logs:/opt/intelmq/var/log \
     -v ~/intelmq-docker/example_config/intelmq/var/lib:/opt/intelmq/var/lib \
