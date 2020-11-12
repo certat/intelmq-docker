3a3c9769b3396d8775d662ea7a34a53d8eace7dc
Sebastian Waldbauer <waldbauer@cert.at>
2020-11-12 15:06:24 +0100

    Fixes #5
    Signed-off-by: Sebastian Waldbauer <waldbauer@cert.at>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $1 == "selftest" ]]
 then
-    cd /opt/intelmq && nosetests3
+    nosetests3 /opt/intelmq/intelmq/tests
 else
     hug -f /opt/intelmq-manager/intelmq_manager/serve.py -p8080
 fi
