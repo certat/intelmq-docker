23fa348319782be2989ce0e2baf0facae2cf9332
Sebastian Waldbauer <waldbauer@cert.at>
2021-01-27 17:59:43 +0100

    [FIX] Exporting variables now
    Signed-off-by: Sebastian Waldbauer <waldbauer@cert.at>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-INTELMQ_IS_DOCKER=1
+export INTELMQ_IS_DOCKER=1
 if [[ $1 == "selftest" ]]
 then
-    INTELMQ_TEST_EXOTIC=1
+    export INTELMQ_TEST_EXOTIC=1
     nosetests3 /opt/intelmq/intelmq/tests
 else
-    INTELMQ_API_CONFIG=/opt/intelmq-api/config/config.json
+    export INTELMQ_API_CONFIG=/opt/intelmq-api/config/config.json
     cd intelmq-api && hug -m intelmq_api.serve -p8080
 fi
