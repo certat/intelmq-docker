4beea486fd2198862540322cad46b52cbd2583a9
Sebastian Waldbauer <waldbauer@cert.at>
2021-08-15 17:27:13 +0200

    FIX: Updated paths in docker container
    Signed-off-by: Sebastian Waldbauer <waldbauer@cert.at>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ export INTELMQ_IS_DOCKER=1
 if [[ $1 == "selftest" ]]
 then
     export INTELMQ_TEST_EXOTIC=1
-    nosetests3 /opt/intelmq/intelmq/tests
+    nosetests3 /etc/intelmq/intelmq/tests
 else
-    export INTELMQ_API_CONFIG=/opt/intelmq-api/config/config.json
-    cd intelmq-api && hug -m intelmq_api.serve -p8080
+    export INTELMQ_API_CONFIG=/etc/intelmq-api/config/config.json
+    cd /etc/intelmq-api && hug -m intelmq_api.serve -p8080
 fi
--- inspect-container.sh
+++ inspect-container.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 docker run --rm -it --entrypoint /bin/bash \
-    -v $(pwd)/example_config/intelmq/etc:/opt/intelmq/etc \
-    -v $(pwd)/example_config/intelmq-api:/opt/intelmq_api/config \
-    -v $(pwd)/intelmq_logs:/opt/intelmq/var/log \
-    -v $(pwd)/example_config/intelmq/var/lib:/opt/intelmq/var/lib \
+    -v $(pwd)/intelmq_persistence:/opt/intelmq_persistence \
+    -v $(pwd)/example_config/intelmq/etc:/etc/intelmq/etc \
+    -v $(pwd)/example_config/intelmq-api/config.json:/etc/intelmq/api-config.json:ro \
+    -v $(pwd)/intelmq_logs:/etc/intelmq/var/log \
+    -v $(pwd)/example_config/intelmq/var/lib:/etc/intelmq/var/lib \
     -e "INTELMQ_IS_DOCKER=\"true\"" \
-    -e "INTELMQ_PIPELINE_DRIVER=\"redis\"" \
-    -e "INTELMQ_PIPELINE_HOST=$redis_ip" \
-    -e "INTELMQ_PIPELINE_AMQ_HOST=$amp_ip" \
-    -e "INTELMQ_REDIS_CACHE_HOST=$redis_ip" \
     intelmq-full:latest
