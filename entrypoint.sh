3df283b5131245996dc30c253147e8a67a3b13fd
Sebastian Waldbauer <waldbauer@cert.at>
2021-08-17 14:31:22 +0200

    FIX: Docker update to fix File permissions
    Signed-off-by: Sebastian Waldbauer <waldbauer@cert.at>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ export INTELMQ_IS_DOCKER=1
 intelmqctl upgrade-config
 intelmqctl check
 
+intelmq_user=${INTELMQ_API_USER:-intelmq}
+intelmq_pass=${INTELMQ_API_PASS:-intelmq}
+intelmq-api-adduser --user $intelmq_user --pass $intelmq_pass
+
 if [[ $1 == "selftest" ]]
 then
     export INTELMQ_TEST_EXOTIC=1
