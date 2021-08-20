514ed8d1e0f141b6e0fc2bd5ac77706226bceaa8
Sebastian Waldbauer <waldbauer@cert.at>
2021-08-20 10:15:53 +0200

    FIX: Escape variables in entrypoints.sh
    Fixes #9
    
    Signed-off-by: Sebastian Waldbauer <waldbauer@cert.at>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,15 @@ export INTELMQ_IS_DOCKER=1
 intelmqctl upgrade-config
 intelmqctl check
 
-intelmq_user=${INTELMQ_API_USER:-intelmq}
-intelmq_pass=${INTELMQ_API_PASS:-intelmq}
-intelmq-api-adduser --user $intelmq_user --pass $intelmq_pass
+intelmq_user="${INTELMQ_API_USER:=intelmq}"
+intelmq_pass="${INTELMQ_API_PASS:=intelmq}"
+
+intelmq-api-adduser --user "$intelmq_user" --password "$intelmq_pass"
 
 if [[ $1 == "selftest" ]]
 then
     export INTELMQ_TEST_EXOTIC=1
     nosetests3 /etc/intelmq/intelmq/tests
 else
-    export INTELMQ_API_CONFIG=/etc/intelmq-api/config/config.json
     cd /etc/intelmq-api && hug -m intelmq_api.serve -p8080
 fi
