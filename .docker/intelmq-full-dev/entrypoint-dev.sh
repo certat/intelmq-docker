9b2a3ef4b5949b801eb1d9da5f795b2128e264bd
Einar Lanfranco <einar.lanfranco@gmail.com>
2022-05-10 18:06:40 -0300

    Adding new entrypoint

---
+++ .docker/intelmq-full-dev/entrypoint-dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export INTELMQ_IS_DOCKER=1
+
+if [[ ${IS_DEV} == "true" ]]
+then
+    /opt/install_reqs_and_deploy_bots.sh
+fi
+
+sudo chown -R intelmq:intelmq /etc/intelmq
+sudo chown -R intelmq:intelmq /opt/intelmq
+
+intelmqctl upgrade-config
+intelmqctl check
+
+intelmq_user="${INTELMQ_API_USER:=intelmq}"
+intelmq_pass="${INTELMQ_API_PASS:=intelmq}"
+
+intelmq-api-adduser --user "$intelmq_user" --password "$intelmq_pass"
+
+if [[ $1 == "selftest" ]]
+then
+    export INTELMQ_TEST_EXOTIC=1
+    nosetests3 /etc/intelmq/intelmq/tests
+else
+    cd /etc/intelmq-api && hug -m intelmq_api.serve -p8080
+fi
